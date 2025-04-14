fbe2b5424364c4a6c9eb381986899dc7c2280115
adimer <adimer101@gmail.com>
2025-04-14 04:26:17 -0400

    Actulizado para modulo de users

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as process from 'node:process';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '3306'),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: process.env.DB_SYNC === 'true',
+      timezone: '-04:00',
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ back/src/users/entities/user.entity.ts
@@ -0,0 +1,53 @@
+import { Exclude, Expose } from 'class-transformer';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+@Unique(['username'])
+export class User {
+  @PrimaryGeneratedColumn()
+  @Expose() // Agregar para asegurar que se incluya en la transformación
+  id: number;
+
+  @Column()
+  @Expose()
+  name: string;
+
+  @Column()
+  @Expose()
+  role: string;
+
+  @Column()
+  @Expose()
+  username: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  @CreateDateColumn()
+  @Exclude()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  @Exclude()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  @Exclude()
+  deletedAt: Date;
+
+  // @OneToMany(() => Sale, (sale) => sale.user)
+  // sales: Sale[];
+  //
+  // @OneToMany(() => Documento, (documento) => documento.user)
+  // documentos: Documento[];
+}
