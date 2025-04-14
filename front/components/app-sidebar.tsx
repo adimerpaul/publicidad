fcd34f30adbc04ed1d46725efdac8a030dbe5886
adimer <adimer101@gmail.com>
2025-04-14 06:30:06 -0400

    Actulizado para recuperacion de nombre

--- front/components/app-sidebar.tsx
+++ front/components/app-sidebar.tsx
@@ -32,132 +32,209 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import {useEffect} from "react";
 
-const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
-  navMain: [
-    {
-      title: "Principal",
-      url: "/dashboard",
-      icon: IconDashboard,
-    },
-    {
-      title: "Usuarios",
-      url: "/dashboard/users",
-      icon: IconUsers,
-    },
-    {
-      title: "Contributentes",
-      url: "/dashboard/contributors",
-      icon: IconUser
-    },
-    {
-      title: "Mapa",
-      url: "/dashboard/map",
-      // icon map
-      icon: IconMap,
-    },
-    {
-      title: "Pagos",
-      url: "/dashboard/payments",
-      // icon de  money
-      icon: IconMoneybag,
-    },
-  ],
-  navClouds: [
-    {
-      title: "Capture",
-      icon: IconCamera,
-      isActive: true,
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
-        },
-        {
-          title: "Archived",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Proposal",
-      icon: IconFileDescription,
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
-        },
-        {
-          title: "Archived",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Prompts",
-      icon: IconFileAi,
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
-        },
-        {
-          title: "Archived",
-          url: "#",
-        },
-      ],
-    },
-  ],
-  navSecondary: [
-    // {
-    //   title: "Settings",
-    //   url: "#",
-    //   icon: IconSettings,
-    // },
-    // {
-    //   title: "Get Help",
-    //   url: "#",
-    //   icon: IconHelp,
-    // },
-    // {
-    //   title: "Search",
-    //   url: "#",
-    //   icon: IconSearch,
-    // },
-    {
-        title: "Salir",
-        url: "#",
-        icon: IconLogout,
-    }
-  ],
-  documents: [
-    {
-      name: "Data Library",
-      url: "#",
-      icon: IconDatabase,
-    },
-    {
-      name: "Reports",
-      url: "#",
-      icon: IconReport,
-    },
-    {
-      name: "Word Assistant",
-      url: "#",
-      icon: IconFileWord,
-    },
-  ],
-}
+
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+
+  // const data = {
+  //   user: {
+  //     name: "shadcn",
+  //     email: "m@example.com",
+  //     avatar: "/avatars/shadcn.jpg",
+  //   },
+  //   navMain: [
+  //     {
+  //       title: "Principal",
+  //       url: "/dashboard",
+  //       icon: IconDashboard,
+  //     },
+  //     {
+  //       title: "Usuarios",
+  //       url: "/dashboard/users",
+  //       icon: IconUsers,
+  //     },
+  //     {
+  //       title: "Contributentes",
+  //       url: "/dashboard/contributors",
+  //       icon: IconUser
+  //     },
+  //     {
+  //       title: "Mapa",
+  //       url: "/dashboard/map",
+  //       // icon map
+  //       icon: IconMap,
+  //     },
+  //     {
+  //       title: "Pagos",
+  //       url: "/dashboard/payments",
+  //       // icon de  money
+  //       icon: IconMoneybag,
+  //     },
+  //   ],
+  //   navClouds: [
+  //     {
+  //       title: "Capture",
+  //       icon: IconCamera,
+  //       isActive: true,
+  //       url: "#",
+  //       items: [
+  //         {
+  //           title: "Active Proposals",
+  //           url: "#",
+  //         },
+  //         {
+  //           title: "Archived",
+  //           url: "#",
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       title: "Proposal",
+  //       icon: IconFileDescription,
+  //       url: "#",
+  //       items: [
+  //         {
+  //           title: "Active Proposals",
+  //           url: "#",
+  //         },
+  //         {
+  //           title: "Archived",
+  //           url: "#",
+  //         },
+  //       ],
+  //     },
+  //     {
+  //       title: "Prompts",
+  //       icon: IconFileAi,
+  //       url: "#",
+  //       items: [
+  //         {
+  //           title: "Active Proposals",
+  //           url: "#",
+  //         },
+  //         {
+  //           title: "Archived",
+  //           url: "#",
+  //         },
+  //       ],
+  //     },
+  //   ],
+  //   navSecondary: [
+  //     // {
+  //     //   title: "Settings",
+  //     //   url: "#",
+  //     //   icon: IconSettings,
+  //     // },
+  //     // {
+  //     //   title: "Get Help",
+  //     //   url: "#",
+  //     //   icon: IconHelp,
+  //     // },
+  //     // {
+  //     //   title: "Search",
+  //     //   url: "#",
+  //     //   icon: IconSearch,
+  //     // },
+  //     {
+  //       title: "Salir",
+  //       url: "#",
+  //       icon: IconLogout,
+  //     }
+  //   ],
+  //   documents: [
+  //     {
+  //       name: "Data Library",
+  //       url: "#",
+  //       icon: IconDatabase,
+  //     },
+  //     {
+  //       name: "Reports",
+  //       url: "#",
+  //       icon: IconReport,
+  //     },
+  //     {
+  //       name: "Word Assistant",
+  //       url: "#",
+  //       icon: IconFileWord,
+  //     },
+  //   ],
+  // }
+  const [data, setData] = React.useState({
+      user: {
+        name: "shadcn",
+        email: "m@example.com",
+        avatar: "/avatars/shadcn.jpg",
+      },
+      navMain: [
+        {
+          title: "Principal",
+          url: "/dashboard",
+          icon: IconDashboard,
+        },
+        {
+          title: "Usuarios",
+          url: "/dashboard/users",
+          icon: IconUsers,
+        },
+        {
+          title: "Contributentes",
+          url: "/dashboard/contributors",
+          icon: IconUser
+        },
+        {
+          title: "Mapa",
+          url: "/dashboard/map",
+          icon: IconMap,
+        },
+        {
+          title: "Pagos",
+          url: "/dashboard/payments",
+          icon: IconMoneybag,
+        },
+      ],
+      navSecondary: [
+        {
+          title: "Salir",
+          url: "#",
+          icon: IconLogout,
+        }
+      ],
+      documents: [
+        {
+          name: "Data Library",
+          url: "#",
+          icon: IconDatabase,
+        },
+        {
+          name: "Reports",
+          url: "#",
+          icon: IconReport,
+        },
+        {
+          name: "Word Assistant",
+          url: "#",
+          icon: IconFileWord,
+        },
+      ],
+  })
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user") || "{}")
+    if (user && user.name) {
+      setData((prev) => ({
+        ...prev,
+        user: {
+          ...prev.user,
+          name: user.username,
+          email: user.name,
+        }
+      }))
+    }
+  }, [])
+
+
   return (
     <Sidebar collapsible="offcanvas" {...props}>
       <SidebarHeader>
@@ -169,7 +246,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             >
               <a href="#">
                 <IconInnerShadowTop className="!size-5" />
-                <span className="text-base font-semibold">Acme Inc.</span>
+                <span className="text-base font-semibold">Publicidad</span>
               </a>
             </SidebarMenuButton>
           </SidebarMenuItem>
